b50159bd86ae071fdd7b8b861b3963b3efd83e47
MadeYoga <vngla21@gmail.com>
2019-05-30 21:15:46 +0700

    Update read html

--- js/read.js
+++ js/read.js
@@ -7,55 +7,59 @@ const __BASE_IMG2__ = "https://cdn.mangaeden.com/mangasimg/98x/";
 
 let pages = [];
 let cursor = 0;
+let pageList = [];
 
 fetch(__BASE_CHAP__ + chapterId + "/")
     .then((resp) => resp.json())
     .then(handlePagesResponse)
     .catch(error);
 
+/**
+ * 
+ * @param {*} data 
+ * data includes page(s) url.
+ */
 function handlePagesResponse(data) {
     console.log(data);
     pages = data.images;
-    cursor = pages.length - 1;
+    pageList = pages;
 
-    pageList = [];
-    for (var i = pages.length - 1; i >= pages.length - 5; i--) {
-        var page = document.createElement('img');
-        page.src = __BASE_IMG2__ + pages[i][1];
-        page.className += "manga-page-image";
-        pageList.push(page);
-    }
+    showPage(4);
 
-    pagesElement = document.getElementById('pages');
-    while (pageList.length > 0) {
-        pagesElement.appendChild(pageList.pop());
-    }
-
-    // var carouselElement = document.getElementById('carousel-page-container');
-    
-    // var imgLink = document.createElement('a');
-    // imgLink.href = "#" + pages[cursor][0];
-    // imgLink.className = 'carousel-item';
-    
-    // var page = document.createElement('img');
-    // page.src = __BASE_IMG2__ + pages[cursor][1];
-
-    // imgLink.appendChild(page);
-    // carouselElement.appendChild(imgLink);
-
-    // the server could explode with this.
-    // for (var i = pages.length - 1; i >= 0; i--) {
-    //     var imgLink = document.createElement('a');
-    //     imgLink.href = "#" + pages[i][0];
-    //     imgLink.className = 'carousel-item';
+    window.onscroll = function() {
+        var d = document.documentElement;
+        var offset = d.scrollTop + window.innerHeight;
+        var height = d.offsetHeight;
         
-    //     var page = document.createElement('img');
-    //     page.src = __BASE_IMG2__ + pages[i][1];
+        if (offset >= height) {
+            console.log('At the bottom');
+            showPage(2);
+        }
+    };
 
-    //     imgLink.appendChild(page);
-    //     carouselElement.appendChild(imgLink);
-    // }
+}
 
+function showPage(number = 5) {
+    if (pageList.length <= 0) {
+        return;
+    }
+    pagesElement = document.getElementById('pages');
+    setTimeout(function(){ loadImage(pagesElement, number - 1) }, 1250);
+    console.log("showing " + number + " number of page.")
+}
+
+function loadImage(pagesElement, number) {
+    if (pageList.length <= 0 || number <= 0) {
+        return;
+    }
+    page = pageList.pop();
+
+    var pageElement = document.createElement('img');
+    pageElement.src = __BASE_IMG2__ + page[1];
+    pageElement.className += "manga-page-image";
+
+    pagesElement.appendChild(pageElement);
+    setTimeout(function(){ loadImage(pagesElement, number - 1) }, 1250);
 }
 
 function error(e) {
